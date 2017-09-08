e8f2bb1da00023d3f12023241bf3263094ed6302
Luis Mendez <zanakz@gmail.com>
2017-09-08 15:56:53 -0500

    Se muestran todos los registros de inscripcion de ingles
    Former-commit-id: 4b997b8a080d52fe6e75cce4694ae31282c01f01

--- app/controllers/inscripcion_registros_controller.rb
+++ app/controllers/inscripcion_registros_controller.rb
@@ -18,7 +18,8 @@ class InscripcionRegistrosController < ApplicationController
       with_curso: CursoNombre.options_for_select
     },
     ) or return
-    @inscripcion_registros = @filterrific.find.order("created_at DESC").where(examen_medio: nil, examen_final: nil).page(params[:pagina])
+    #@inscripcion_registros = @filterrific.find.order("created_at DESC").where(examen_medio: nil, examen_final: nil).page(params[:pagina])
+    @inscripcion_registros = @filterrific.find.order("created_at DESC").page(params[:pagina])
 
     respond_to do |format|
       format.html
