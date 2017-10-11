b2a961e254ddcc9424171c5e91cb136dac9d7371
Luis Mendez <zanakz@gmail.com>
2017-10-11 18:25:20 -0500

    Se arreglo el bug que no permitia guardar calificaciones en los grupos de italiano
    Former-commit-id: d618287b4bf0e0812014863193dd31578c370241

--- app/models/grupos_unitario.rb
+++ app/models/grupos_unitario.rb
@@ -1,7 +1,9 @@
 class GruposUnitario < ApplicationRecord
 
   has_many :unitarios
-  
+
+  paginates_per 25
+
   def jefe_educacion_continua
     rol = Role.find_by(nombre: "Jefe Educación Continua").id
     usuarios = User.find_by(role: rol).nombre_paterno_materno
