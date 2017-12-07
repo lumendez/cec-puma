3f824b3d184491e1d77ad395983be558afeb9c70
Luis Mendez <zanakz@gmail.com>
2017-12-06 18:43:54 -0600

    Ajustes finales para el filtro por nombre
    Former-commit-id: e1740762cee71a4ace8c7fc89b54140149de5d37

--- app/models/unitario.rb
+++ app/models/unitario.rb
@@ -97,8 +97,8 @@ class Unitario < ApplicationRecord
     where(grupos_unitario_id: [*grupos_unitario_ids])
   }
 
-  scope :with_grupos_creados_id, lambda { |grupos_creados_ids|
-    where(grupos_creados_id: [*grupos_creados_ids])
+  scope :with_grupos_creados_id, lambda { |grupos_unitario_ids|
+    where(grupos_unitario_id: [*grupos_unitario_ids])
   }
 
   scope :with_documentos_validados, lambda { |documentos_validados|
