87f91b5eac9096725373240d177beb586f191553
Luis Mendez <zanakz@gmail.com>
2018-01-31 20:12:30 -0600

    Filtro para examenes de colocacion y paginacion
    Former-commit-id: 58e948ef2da24e57af73f57c5ca5e5ece5457e5a

--- app/models/examen_colocacion_idioma.rb
+++ app/models/examen_colocacion_idioma.rb
@@ -3,6 +3,14 @@ class ExamenColocacionIdioma < ApplicationRecord
   has_attached_file :imagen
   validates_attachment_content_type :imagen, :content_type => /\Aimage\/.*\Z/
 
+  filterrific(
+    available_filters: [
+      :search_query
+    ]
+  )
+
+  paginates_per 35
+
   belongs_to :user
 
   def nombre_completo
@@ -12,5 +20,32 @@ class ExamenColocacionIdioma < ApplicationRecord
   def fecha_expiracion
     self.created_at.advance(weeks: 8).strftime("%d/%m/%y")
   end
+
+  scope :search_query, lambda { |query|
+
+    # Filters students whose name or email matches the query
+    return nil  if query.blank?
+
+    # condition query, parse into individual keywords
+    terms = query.downcase.split(/\s+/)
+
+    # replace "*" with "%" for wildcard searches,
+    # append '%', remove duplicate '%'s
+    terms = terms.map { |e|
+      (e.gsub('*', '%') + '%').gsub(/%+/, '%')
+    }
+
+    # configure number of OR conditions for provision
+    # of interpolation arguments. Adjust this if you
+    # change the number of OR conditions.
+    num_or_conds = 3
+
+    where(
+      terms.map { |term|
+        "(LOWER(examen_colocacion_idiomas.nombre) LIKE ? OR LOWER(examen_colocacion_idiomas.paterno) LIKE ? OR LOWER(examen_colocacion_idiomas.materno) LIKE ?)"
+      }.join(' AND '),
+      *terms.map { |e| [e] * num_or_conds }.flatten
+    )
+  }
 
 end
