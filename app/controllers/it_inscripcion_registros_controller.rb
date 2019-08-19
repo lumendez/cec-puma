190be34c2d738bd4e1ab70b46d689048b01dd912
luis <luis@localhost.localdomain>
2019-08-19 11:52:42 -0700

    arregladas las cuotas para alumnos

--- app/controllers/it_inscripcion_registros_controller.rb
+++ app/controllers/it_inscripcion_registros_controller.rb
@@ -39,6 +39,9 @@ class ItInscripcionRegistrosController < ApplicationController
 
   # GET /it_inscripcion_registros/new
   def new
+    #Se obtienen los montos actualizados de las cuotas.
+    @cuota_ipn = CuotaCurso.find_by(descripcion: "Cuota IPN").nombre
+    @cuota_externos = CuotaCurso.find_by(descripcion: "Cuota externos").nombre
     #Definición de las condiciones para un usuario pueda registrarse. @registro_anterior
     #corresponde al último registro que haya tenido el usuario en el sistema. @registros_no_aprobados
     #cuenta los registro que el usuario tenga previamente reprobados y examen_colocacion si tiene
--- app/controllers/panel_alumnos_controller.rb
+++ app/controllers/panel_alumnos_controller.rb
@@ -6,5 +6,9 @@ class PanelAlumnosController < ApplicationController
     @fr_inscripcion_registros = FrInscripcionRegistro.where("user_id = ?", current_user.id )
     @it_inscripcion_registros = ItInscripcionRegistro.where("user_id = ?", current_user.id )
     @examen_colocacion_idiomas = ExamenColocacionIdioma.where("user_id = ?", current_user.id )
+    @cuota_ipn = CuotaCurso.find_by(descripcion: "Cuota IPN").nombre
+    @cuota_externos = CuotaCurso.find_by(descripcion: "Cuota externos").nombre
+    @cuota_ipn_cert = CuotaCurso.find_by(descripcion: "Certificación IPN").nombre
+    @cuota_externos_cert = CuotaCurso.find_by(descripcion: "Certificación externos").nombre
   end
 end
