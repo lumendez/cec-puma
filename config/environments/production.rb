904d5c5121f9865ccbd683fc59df9871e143bb49
luis <luis@localhost.localdomain>
2019-08-23 16:17:21 -0700

    configuracion del mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,13 +90,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.smtp_settings = {
-    address: "correo.ipn.mx",
-    port: 587,
-    domain: "ACTIVEDIRECTORY",
-    authentication: "login",
-    enable_starttls_auto: true,
-    user_name: ENV['ipn_username'],
-    password: ENV['ipn_password']
-}
+
 end
